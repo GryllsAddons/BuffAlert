f9aa9218b7fb8c31987580ab06a0b6516c5e2d7f
GryllsAddons <107083057+GryllsAddons@users.noreply.github.com>
2023-03-27 19:23:09 +0100

    Add files via upload

--- BuffAlert.lua
+++ BuffAlert.lua
@@ -1,13 +1,14 @@
-local selfbuffs = CreateFrame("Frame", nil, UIParent)
-selfbuffs:RegisterEvent("CHAT_MSG_SPELL_PERIODIC_SELF_BUFFS")
-selfbuffs:RegisterEvent("CHAT_MSG_SPELL_SELF_BUFF")
+local events = CreateFrame("Frame", nil, UIParent)
+events:RegisterEvent("CHAT_MSG_SPELL_PERIODIC_SELF_BUFFS")
+events:RegisterEvent("CHAT_MSG_SPELL_SELF_BUFF")
+events:RegisterEvent("CHAT_MSG_SPELL_SELF_DAMAGE")
 
 local messages = {
     "You gain Windfury Weapon.",
-    "(.*)extra attack through Windfury Totem(.*)"
+    "(.*)through Windfury Totem."
 }
 
-selfbuffs:SetScript("OnEvent", function()
+events:SetScript("OnEvent", function()
     for _, msg in pairs(messages) do
         if (string.find(arg1, msg)) then
             PlaySoundFile[[Interface\AddOns\BuffAlert\sound.ogg]]
